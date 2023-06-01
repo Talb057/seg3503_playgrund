8305e0f8bdc378a4007cb0eacb97bd6cbb6b06e2
Ali <alitalbe33@gmail.com>
2023-06-01 19:19:40 -0400

    added pictures

--- LAB 2/ecs/ecs/test/DateNextDateExceptionTest.java
+++ LAB 2/ecs/ecs/test/DateNextDateExceptionTest.java
@@ -1,5 +1,47 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+
+import java.util.*;
+
+@RunWith(Parameterized.class)
 public class DateNextDateExceptionTest
 {
+    private int year;
+    private int month;
+    private int day;
+    
+    public DateNextDateExceptionTest(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
 
+    @Parameterized.Parameters
+    public static List<Integer []> data() {
+        List<Integer []> inputAndOutputs = new ArrayList<Integer[]> ();
+        inputAndOutputs.add(new Integer[] {1700, 06, 20});
+        inputAndOutputs.add(new Integer[] {1500, 2, 29});
+        inputAndOutputs.add(new Integer[] {2005, 04, 15});
+        inputAndOutputs.add(new Integer[] {1901, 07, 21});
+        inputAndOutputs.add(new Integer[] {1800, 11, 29});
+        inputAndOutputs.add(new Integer[] {1, 1, 32});
+        inputAndOutputs.add(new Integer[] {1, 11, 31});
+        inputAndOutputs.add(new Integer[] {2020, 2, 30});
+        inputAndOutputs.add(new Integer[] {1975, 6, -50});
+        return inputAndOutputs;
+    }
+
+    @Test
+    public void testNextDateCorrect() throws IllegalArgumentException {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Date d = new Date(this.year, this.month, this.day).nextDate();
+        });
+    }
 
 }
--- LAB 2/ecs/ecs/test/DateNextDateOkTest.java
+++ LAB 2/ecs/ecs/test/DateNextDateOkTest.java
@@ -1,5 +1,44 @@
-public class DateNextDateOkTest
-{
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import static org.junit.jupiter.api.Assertions.*;
 
 
+import java.util.*;
+
+@RunWith(Parameterized.class)
+public class DateNextDateOkTest {
+    
+    private Date input;
+    private Date output;
+    
+    public DateNextDateOkTest(Date input, Date output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Parameterized.Parameters(name="{index}: testDate_NextDate({0})={2}")
+    public static List<Date []> data() {
+        //List<Date [][]> inputAndOutputs = new ArrayList<Date [][]> ();
+        
+        return Arrays.asList(new Date[][] {
+                {new Date(1366, 3, 4), new Date(2363, 2, 23)},
+                {new Date(777, 8, 8), new Date(777, 8, 9)},
+                {new Date(777, 4, 30), new Date(777, 3, 2)},
+                {new Date(1999, 3, 32), new Date(999, 6, 2)},
+                {new Date(1700, 06, 20), new Date(2700, 06, 22)},
+                {new Date(1003, 04, 23), new Date(2003, 04, 26)},
+                {new Date(1902, 07, 20), new Date(2902, 07, 22)},
+                {new Date(1436, 3, 27), new Date(3436, 3, 28)},
+                {new Date(1300, 2, 27), new Date(2300, 2, 28)},
+                {new Date(1700, 6, 29), new Date(2700, 6, 30)},
+                {new Date(1436, 22, 32), new Date(3437, 2, 2)}
+        });
+    }
+
+    @Test
+    public void testNextDateCorrect() {
+        assertEquals(input.nextDate(), output);
+    }
 }
